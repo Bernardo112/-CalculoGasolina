1c75953719df240bf63e6b821a2ae16f8bbcf3eb
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-03-15 02:52:21 +0900

    Implementing conditions and background image

--- www/components/amend.js
+++ www/components/amend.js
@@ -1,9 +1,20 @@
-// This is a JavaScript file
-
 function GasolinaCalculo(){
-    consumo = document.getElementById('KM').value / document.getElementById('Gasolina').value;
-    marca = document.getElementById('inputGroupSelect01').value;
-    modelo = document.getElementById('Modelo').value;
+    var consumo = document.getElementById('KM').value / document.getElementById('Gasolina').value;
+    var marca = document.getElementById('inputGroupSelect01').value;
+    var modelo = document.getElementById('Modelo').value;
 
-    document.getElementById('resultado').innerHTML = consumo + " KM/L";
+    if(consumo >= 20){
+        document.getElementById('economia').innerHTML = '<border style="border-style: dotted; border-color: red;">Extremamente Econômico</border>';
+    }
+    else if (consumo >=12 && consumo < 20){
+        document.getElementById('economia').innerHTML = '<border style="border-style: dotted; border-color: red;">Econômico</border>';
+    }
+    else if (consumo >= 8 && consumo < 12){
+        document.getElementById('economia').innerHTML = '<border style="border-style: dotted; border-color: red;">Normal</border>';
+    }
+    else if (consumo < 8){
+        document.getElementById('economia').innerHTML = '<border style="border-style: dotted; border-color: red;">Alto Consumo</border>';
+    }
+
+    document.getElementById('resultado').innerHTML = "O seu carro de marca " + marca + " e de modelo " + modelo + " tem consumo de "+ consumo + " KM/L";
 }
